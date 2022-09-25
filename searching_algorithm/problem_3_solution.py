3753e698d3929c74e061cec4564f2c7bca9151d5
Royalboe <auomidejohnson@gmail.com>
2022-09-25 14:35:43 +0100

    linear solution

--- searching_algorithm/problem_3_solution.py
+++ searching_algorithm/problem_3_solution.py
@@ -5,5 +5,25 @@ Your function should have the worst-case complexity of `O(log N)`, where N is th
 You can assume that all the numbers in the list are unique.
 '''
 
-def count_rotations(rotated_list):
-    'TODOS'
+def count_rotations_linear(rotated_list):
+    position, counts, length = 0, 0, len(rotated_list)
+    if length == 0:
+        return -1
+    while position < length:
+        if rotated_list[position] > rotated_list[position + 1]:
+            counts += 1
+            return counts
+        else:
+            position += 1
+            counts += 1
+    return 0
+
+def print_result(rotated_list):
+    counts = count_rotations_linear(rotated_list)
+
+    if counts == 0:
+        print(f"List was not rotated")
+    elif counts > 0:
+        print(f"List was rotated at least {counts} times")
+    else:
+        print("List is empty")
